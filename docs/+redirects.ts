0ca22889474fa21a92be202b106335b5d3f1661d
Romuald Brillout <git@brillout.com>
2026-06-30 23:19:50 +0200

    revert brillout docs changes

--- docs/+redirects.ts
+++ docs/+redirects.ts
@@ -11,7 +11,4 @@ checkType<HeadingsURL>(0 as any as RedirectsURL)
 
 const redirects = {
   '/remix': '/react-router',
-  '/httpHeaders': '/headers',
-  '/telefunc': '/serve',
-  '/server': '/Telefunc',
 } as const satisfies Config['redirects']
